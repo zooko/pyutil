d0244e1dd6fdcba5cf8c4265f3392248e826819e
Zooko Wilcox-O'Hearn <zooko@zooko.com>
2014-03-17 22:00:04 +0000

    install new versioneer

--- setup.py
+++ setup.py
@@ -11,81 +11,75 @@ import os, re, sys
 from setuptools import find_packages, setup
 
 trove_classifiers=[
-    "Development Status :: 5 - Production/Stable",
-    "License :: OSI Approved :: GNU General Public License (GPL)",
-    "License :: DFSG approved",
-    "Intended Audience :: Developers",
-    "Operating System :: Microsoft :: Windows",
-    "Operating System :: Unix",
-    "Operating System :: MacOS :: MacOS X",
-    "Operating System :: OS Independent",
-    "Natural Language :: English",
-    "Programming Language :: Python",
-    "Programming Language :: Python :: 2",
-    "Programming Language :: Python :: 2.7",
-    "Topic :: Utilities",
-    "Topic :: Software Development :: Libraries",
+    u"Development Status :: 5 - Production/Stable",
+    u"License :: OSI Approved :: GNU General Public License (GPL)",
+    u"License :: DFSG approved",
+    u"Intended Audience :: Developers",
+    u"Operating System :: Microsoft :: Windows",
+    u"Operating System :: Unix",
+    u"Operating System :: MacOS :: MacOS X",
+    u"Operating System :: OS Independent",
+    u"Natural Language :: English",
+    u"Programming Language :: Python",
+    u"Programming Language :: Python :: 2",
+    u"Programming Language :: Python :: 2.7",
+    u"Topic :: Utilities",
+    u"Topic :: Software Development :: Libraries",
     ]
 
-PKG='pyutil'
-VERSIONFILE = os.path.join(PKG, "_version.py")
-verstr = "unknown"
-try:
-    verstrline = open(VERSIONFILE, "rt").read()
-except EnvironmentError:
-    pass # Okay, there is no version file.
-else:
-    VSRE = r"^verstr = ['\"]([^'\"]*)['\"]"
-    mo = re.search(VSRE, verstrline, re.M)
-    if mo:
-        verstr = mo.group(1)
-    else:
-        print "unable to find version in %s" % (VERSIONFILE,)
-        raise RuntimeError("if %s.py exists, it must be well-formed" % (VERSIONFILE,))
+PKG=u'pyutil'
+VERSIONFILE = os.path.join(PKG, u"_version.py")
 
-doc_fnames=[ 'COPYING.SPL.txt', 'COPYING.GPL', 'COPYING.TGPPL.rst', 'README.rst', 'CREDITS' ]
+import versioneer
+versioneer.versionfile_source = VERSIONFILE
+versioneer.versionfile_build = VERSIONFILE
+versioneer.tag_prefix = PKG+u'-' # tags are like pyutil-1.2.0
+versioneer.parentdir_prefix = PKG+u'-' # dirname like 'myproject-1.2.0'
+
+doc_fnames=[ u'COPYING.SPL.txt', u'COPYING.GPL', u'COPYING.TGPPL.rst', u'README.rst', u'CREDITS' ]
 
 # In case we are building for a .deb with stdeb's sdist_dsc command, we put the
 # docs in "share/doc/python-$PKG".
-doc_loc = "share/doc/" + PKG
+doc_loc = u"share/doc/" + PKG
 
 data_files = [
     (doc_loc, doc_fnames),
-    (os.path.join('pyutil', 'data'), [os.path.join('pyutil', 'data', 'wordlist.txt')])
+    (os.path.join(u'pyutil', u'data'), [os.path.join(u'pyutil', u'data', u'wordlist.txt')])
     ]
 
-install_requires=['zbase32 >= 1.0']
+install_requires=[u'zbase32 >= 1.0']
 
-readmetext_bytes = open('README.rst').read()
+readmetext_bytes = open(u'README.rst').read()
 readmetext_unicode = readmetext_bytes.decode('utf-8')
 while readmetext_unicode[0] == u'\ufeff':
     readmetext_unicode = readmetext_unicode[1:]
 
 setup(name=PKG,
-      version=verstr,
-      description='a collection of utilities for Python programmers',
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
+      description=u'a collection of utilities for Python programmers',
       long_description=readmetext_unicode,
-      author="Zooko Wilcox-O'Hearn",
-      author_email='zookog@gmail.com',
-      url='https://tahoe-lafs.org/trac/' + PKG,
-      license='GNU GPL', # see README.rst for details -- there are also alternative licences
+      author=u"Zooko Wilcox-O'Hearn",
+      author_email=u'zookog@gmail.com',
+      url=u'https://tahoe-lafs.org/trac/' + PKG,
+      license=u'GNU GPL', # see README.rst for details -- there are also alternative licences
       packages=find_packages(),
       include_package_data=True,
       data_files=data_files,
-      extras_require={'jsonutil': ['simplejson >= 2.1.0',]},
+      extras_require={u'jsonutil': [u'simplejson >= 2.1.0',]},
       install_requires=install_requires,
       classifiers=trove_classifiers,
       entry_points = {
-          'console_scripts': [
-              'randcookie = pyutil.scripts.randcookie:main',
-              'tailx = pyutil.scripts.tailx:main',
-              'lines = pyutil.scripts.lines:main',
-              'randfile = pyutil.scripts.randfile:main',
-              'unsort = pyutil.scripts.unsort:main',
-              'verinfo = pyutil.scripts.verinfo:main',
-              'try_decoding = pyutil.scripts.try_decoding:main',
-              'passphrase = pyutil.scripts.passphrase:main',
+          u'console_scripts': [
+              u'randcookie = pyutil.scripts.randcookie:main',
+              u'tailx = pyutil.scripts.tailx:main',
+              u'lines = pyutil.scripts.lines:main',
+              u'randfile = pyutil.scripts.randfile:main',
+              u'unsort = pyutil.scripts.unsort:main',
+              u'verinfo = pyutil.scripts.verinfo:main',
+              u'try_decoding = pyutil.scripts.try_decoding:main',
+              u'passphrase = pyutil.scripts.passphrase:main',
               ] },
-      test_suite=PKG+".test",
+      test_suite=PKG+u".test",
       zip_safe=False, # I prefer unzipped for easier access.
       )
